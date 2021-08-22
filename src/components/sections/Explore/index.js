9bf15c53b0680babe15efeda09ea63882f34716d
mosa-rabea <mosa@protonmail.com>
2021-08-22 12:36:14 +0300

    test

--- src/components/sections/Explore/index.js
+++ src/components/sections/Explore/index.js
@@ -3,7 +3,7 @@ import { Element } from 'react-scroll';
 import './HeroSection.css';
 import ScrollOnView from '../../common/ScrollOnView';
 import { withAuth0 } from '@auth0/auth0-react';
-import {Container,Row,Col} from 'react-bootstrap';
+import {Container,Row} from 'react-bootstrap';
 class Explore extends Component {
   render() {
     return (
@@ -13,11 +13,7 @@ class Explore extends Component {
           <ScrollOnView reverse>
             <Container>
               <Row>
-                <Col>
-                  <h1 className="c-HeroSection__title">
-                        Explore Section !
-                  </h1>
-                </Col>
+
               </Row>
             </Container>
           </ScrollOnView>
