2407835e1f11b89b7a4c885c2bb3a6a559b50617
mosa-rabea <mosa@protonmail.com>
2021-08-23 21:36:01 +0300

    renderEvreryThing

--- src/components/sections/Hero/Modal/SportModal.js
+++ src/components/sections/Hero/Modal/SportModal.js
@@ -6,6 +6,7 @@ import {
   Dropdown,
   DropdownButton,
 } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import './SportModal.css';
 import { withAuth0 } from '@auth0/auth0-react';
@@ -13,59 +14,107 @@ import countriesdata from './res/countries.json';
 import axios from 'axios';
 
 class SportModal extends Component {
-  constructor( props ) {
-    super( props );
+  constructor(props) {
+    super(props);
     this.state = {
       currentStep: 1,
       leaguesData: [],
       allTeamsData: [],
-      nickname:'',
+      nickname: '',
       favouriteleague: '',
       favTeamName: '',
       showModal: true,
       selectedSport: '',
       user: this.props.userdata,
+      search: '',
     };
   }
- 
 
-  handleChange = ( event ) => {
-    const { name, value } = event.target;
-    this.setState( {
-      [name]: value,
-    } );
+  renderCountry = (country) => {
+    const { search } = this.state;
+
+    if (
+      search !== '' &&
+      country.name.toLowerCase().indexOf(search.toLowerCase()) === -1
+    ) {
+      return null;
+    }
+
+    return (
+      <>
+        <Dropdown.Item
+          as='button'
+          onClick={(e) => this.getLeaguesData(e)}
+          value={country.name}
+        >
+          {country.name}{' '}
+        </Dropdown.Item>
+      </>
+    );
   };
 
-  handleSubmit = ( e ) => {
-    console.log ( this.props.userdata._id ) ;
+  handelsearch = (e) => {
+    this.setState({ search: e.target.value });
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = (e) => {
     e.preventDefault();
-    const { nickname, favouriteleague, favTeamName,selectedSport } = this.state;
-    
-    axios.patch( `http://localhost:3050/updateUser/${this.props.userdata._id}`,
-      {favTeamName,nickname,favouriteleague,selectedSport} )
-      .then( resp=>{
-        console.log( resp.data );
-      } );  };
+    const { nickname, favouriteleague, favTeamName, selectedSport } =
+      this.state;
+    //     alert( `Your registration detail: \n
+    //              nickname: ${nickname} \n
+    //              favouriteLeague: ${favouriteleague} \n
+    //              selectedSport: ${selectedSport} \n
+    //              favTeam: ${favTeamName}` );
+    //     let test = {
+    //       'nickname': '000000000',
+    //       'favouriteleague' : '000000000',
+    //       'favTeamName' : '000000000' ,
+    //       'selectedSport': '00000000'
+
+    //     };
+    if (this.props.auth0.isAuthenticated) {
+      const config = {
+        method: 'patch',
+        baseURL: 'http://localhost:3050',
+        body: { nickname, favouriteleague, favTeamName, selectedSport },
+        url: `/updateUser/${this.state.user._id}`,
+      };
+      axios(config)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => console.error(err));
+    }
+    this.handleClose();
+  };
 
   _next = () => {
     let currentStep = this.state.currentStep;
     currentStep = currentStep >= 2 ? 3 : currentStep + 1;
-    this.setState( {
+    this.setState({
       currentStep: currentStep,
-    } );
+    });
   };
 
   _prev = () => {
     let currentStep = this.state.currentStep;
     currentStep = currentStep <= 1 ? 1 : currentStep - 1;
-    this.setState( {
+    this.setState({
       currentStep: currentStep,
-    } );
+    });
   };
 
   previousButton() {
     let currentStep = this.state.currentStep;
-    if ( currentStep !== 1 ) {
+    if (currentStep !== 1) {
       return (
         <button
           className='btn btn-secondary'
@@ -81,7 +130,7 @@ class SportModal extends Component {
 
   nextButton() {
     let currentStep = this.state.currentStep;
-    if ( currentStep < 3 && currentStep > 1 ) {
+    if (currentStep < 3 && currentStep > 1) {
       return (
         <button
           className='btn btn-primary float-right'
@@ -95,23 +144,23 @@ class SportModal extends Component {
     return null;
   }
 
-  sportSelected = ( sport ) => {
-    this.setState( {
+  sportSelected = (sport) => {
+    this.setState({
       selectedSport: sport,
-    } );
+    });
     this._next();
   };
   handleClose = () => {
-    this.setState( { showModal: false } );
+    this.setState({ showModal: false });
   };
 
   //// Api Requests
-  getLeaguesData = async( e ) => {
+  getLeaguesData = async (e) => {
     e.preventDefault();
-    if ( this.props.auth0.isAuthenticated ) {
+    if (this.props.auth0.isAuthenticated) {
       this.props.auth0
         .getIdTokenClaims()
-        .then( ( token ) => {
+        .then((token) => {
           const jwt = token.__raw;
           const config = {
             headers: { Authorization: `Bearer ${jwt}` },
@@ -119,23 +168,24 @@ class SportModal extends Component {
             baseURL: 'http://localhost:3050',
             url: `/leagues/${e.target.value}`,
           };
-          axios( config )
-            .then( ( response ) => {
-              this.setState( {
+          axios(config)
+            .then((response) => {
+              this.setState({
                 leaguesData: response.data,
-              } );
-              console.log( this.state.leaguesData );
-            } ).catch( ( err ) => console.error( err ) );
-        } )
-        .catch( ( err ) => console.error( err ) );
+              });
+              console.log(this.state.leaguesData);
+            })
+            .catch((err) => console.error(err));
+        })
+        .catch((err) => console.error(err));
     }
   };
-  getTeamsData = async( e ) => {
+  getTeamsData = async (e) => {
     e.preventDefault();
-    if ( this.props.auth0.isAuthenticated ) {
+    if (this.props.auth0.isAuthenticated) {
       this.props.auth0
         .getIdTokenClaims()
-        .then( ( token ) => {
+        .then((token) => {
           const jwt = token.__raw;
           const config = {
             headers: { Authorization: `Bearer ${jwt}` },
@@ -143,26 +193,26 @@ class SportModal extends Component {
             baseURL: 'http://localhost:3050',
             url: `/teams/${e.target.value}`,
           };
-          axios( config )
-            .then( ( response ) => {
-              this.setState( {
+          axios(config)
+            .then((response) => {
+              this.setState({
                 favouriteleague: e.target.value,
                 allTeamsData: response.data,
-              } );
-            } ).catch( ( err ) => console.error( err ) );
-        } )
-        .catch( ( err ) => console.error( err ) );
+              });
+            })
+            .catch((err) => console.error(err));
+        })
+        .catch((err) => console.error(err));
     }
   };
-  getOneTeamData = async( e ) => {
+  getOneTeamData = async (e) => {
     e.preventDefault();
-    await this.setState( {
+    await this.setState({
       favTeamName: e.target.value,
-    } );
+    });
     this._next();
-    console.log( this.state.favTeamName, this.state.favouriteleague );
+    console.log(this.state.favTeamName, this.state.favouriteleague);
   };
-
 
   render() {
     return (
@@ -217,8 +267,8 @@ class SportModal extends Component {
   }
 }
 
-function Step1( props ) {
-  if ( props.currentStep !== 1 ) {
+function Step1(props) {
+  if (props.currentStep !== 1) {
     return null;
   }
   return (
@@ -230,7 +280,7 @@ function Step1( props ) {
         </div>
         <Col>
           <img
-            onClick={() => props.sportSelected( 'soccer' )}
+            onClick={() => props.sportSelected('soccer')}
             src='https://static.toiimg.com/thumb/msid-70661134,imgsize-761205,width-800,height-600,resizemode-75/70661134.jpg'
             className='card-img'
             alt='football'
@@ -238,7 +288,7 @@ function Step1( props ) {
         </Col>
         <Col>
           <img
-            onClick={() => props.sportSelected( 'basketball' )}
+            onClick={() => props.sportSelected('basketball')}
             src='https://cdn.britannica.com/51/190751-050-147B93F7/soccer-ball-goal.jpg'
             className='card-img'
             alt='football'
@@ -252,72 +302,82 @@ function Step1( props ) {
   );
 }
 
-function Step2( props ) {
-  if ( props.currentStep !== 2 ) {
+function Step2(props) {
+  if (props.currentStep !== 2) {
     return null;
   }
   return (
     <>
       <div className='form-group'>
-      
         <label>Select Your Favourite league & Team </label>
         <Row>
           <Col>
+            <label>Country Name :</label>
+            <input
+              label='Search Country'
+              icon='search'
+              onChange={(e) => this.handelsearch(e)}
+            />
+          </Col>
+          <Col>
             <Dropdown>
-              <DropdownButton id='dropdown-item-button' title={'countries'}>
-                {countriesdata.map( ( element,index ) => (
-                  <Dropdown.Item
-                    as='button'
-                    onClick={( e ) => props.getLeaguesData( e )}
-                    value={element.name}
-                    key={index}
-                  >
-                    {element.name}
-                  </Dropdown.Item>
-                ) )}
+              <DropdownButton
+                variant='warning'
+                id='dropdown-item-button'
+                title={'countries'}
+              >
+                {countriesdata.map((country) => {
+                  return this.renderCountry(country);
+                })}
               </DropdownButton>
             </Dropdown>
           </Col>
           <Col>
-            {( props.leaguesData.length > 0 && props.leaguesData[0] !== 'No Leagues' ) && (
-              <Dropdown>
-                <DropdownButton id='dropdown-item-button' title={'Leagues'}>
-                  {props.leaguesData.length > 0 &&
-                  props.leaguesData.map( ( element ) => (
-                    <Dropdown.Item
-                      as='button'
-                      key={element.leagueId}
-                      onClick={( e ) => props.getTeamsData( e )}
-                      value={element.leagueId}
-                    >
-                      {element.leagueName}
-                    </Dropdown.Item>
-                  ) )}
-                </DropdownButton>
-              </Dropdown>
-
-            )}
+            {props.leaguesData.length > 0 &&
+              props.leaguesData[0] !== 'No Leagues' && (
+                <Dropdown>
+                  <DropdownButton
+                    variant='warning'
+                    id='dropdown-item-button'
+                    title={'Leagues'}
+                  >
+                    {props.leaguesData.length > 0 &&
+                      props.leaguesData.map((element) => (
+                        <Dropdown.Item
+                          as='button'
+                          key={element.leagueId}
+                          onClick={(e) => props.getTeamsData(e)}
+                          value={element.leagueId}
+                        >
+                          {element.leagueName}
+                        </Dropdown.Item>
+                      ))}
+                  </DropdownButton>
+                </Dropdown>
+              )}
           </Col>
           <Col>
             {props.allTeamsData.length > 0 && (
-
               <Dropdown>
-                <DropdownButton id='dropdown-item-button' title={'Teams'}>
+                <DropdownButton
+                  variant='warning'
+                  id='dropdown-item-button'
+                  title={'Teams'}
+                >
                   <>
-                    {props.allTeamsData.map( ( element,index ) => (
+                    {props.allTeamsData.map((element, index) => (
                       <Dropdown.Item
-                        onClick={( e ) => props.getOneTeamData( e )}
+                        onClick={(e) => props.getOneTeamData(e)}
                         as='button'
                         key={index}
                         value={element.teamNameDetails}
                       >
                         {element.teamNameDetails}
                       </Dropdown.Item>
-                    ) )}
+                    ))}
                   </>
                 </DropdownButton>
               </Dropdown>
-
             )}
           </Col>
         </Row>
@@ -326,8 +386,8 @@ function Step2( props ) {
   );
 }
 
-function Step3( props ) {
-  if ( props.currentStep !== 3 ) {
+function Step3(props) {
+  if (props.currentStep !== 3) {
     return null;
   }
   return (
@@ -349,4 +409,4 @@ function Step3( props ) {
   );
 }
 
-export default withAuth0( SportModal );
+export default withAuth0(SportModal);
